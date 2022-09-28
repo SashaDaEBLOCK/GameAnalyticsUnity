24def6c58b9c3707409e3639f43e6940beb4b18c
SashaDaEBLOCK <sasha.de.block@student.howest.be>
2022-09-28 16:58:50 +0200

    Converted project to WEBGL
    Converted for easier usage on itch.io down the line.

--- Game Analytics/Assets/Scripts/SendAnalyticsEvent.cs
+++ Game Analytics/Assets/Scripts/SendAnalyticsEvent.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class SendAnalyticsEvent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void SendAnalytics()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Send Analytics");
     }
 }
