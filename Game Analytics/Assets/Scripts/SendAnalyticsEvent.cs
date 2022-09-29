38ff553f0a4cefac4a733833b2da15b9384f8c55
SashaDaEBLOCK <sasha.de.block@student.howest.be>
2022-09-29 12:49:11 +0200

    tried importing the game analytics
    Tried importing trough the .manifest rather then using the unity package manager

--- Game Analytics/Assets/Scripts/SendAnalyticsEvent.cs
+++ Game Analytics/Assets/Scripts/SendAnalyticsEvent.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GameAnalyticsSDK;
 
 public class SendAnalyticsEvent : MonoBehaviour
 {
+    string eventName = "Button presses.";
+    float eventValue;
     public void SendAnalytics()
     {
         Debug.Log("Send Analytics");
+        GameAnalytics.NewDesignEvent(eventName, eventValue++);
     }
 }
